50ed22c1166c3cbb0c0194ae2d6975e9ba73902f
elhernes <eric@qinc.tv>
2019-11-29 09:38:12 -0600

    revert shaders

--- config/camotics/resources.cpp
+++ config/camotics/resources.cpp
@@ -9,9 +9,9 @@ using namespace cb;
 
 namespace CAMotics {
 extern const unsigned char data2[];
-extern const FileResource resource2("phong.frag", (const char *)data2, 577);
+extern const FileResource resource2("phong.frag", (const char *)data2, 554);
 extern const unsigned char data3[];
-extern const FileResource resource3("phong.vert", (const char *)data3, 471);
+extern const FileResource resource3("phong.vert", (const char *)data3, 448);
 const Resource *children1[] = {&resource2,&resource3,0};
 extern const DirectoryResource resource1("shaders", children1);
 const Resource *children0[] = {&resource1,0};
